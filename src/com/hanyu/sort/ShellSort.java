6242070b25043147d5ada3cab30f2f6924edc1ac
hanyu <hanyu@itcast>
2023-10-23 21:42:13 +0800

    移位式的希尔排序

--- src/com/hanyu/sort/ShellSort.java
+++ src/com/hanyu/sort/ShellSort.java
@@ -2,7 +2,7 @@ package com.hanyu.sort;
 
 public class ShellSort {
     public static void main(String[] args) {
-        int[] arr = new int[80000];
+        int[] arr = new int[8000000];
         for (int i = 0; i < arr.length; i++) {
             // 随机数为0-80000
             arr[i] = (int) (Math.random() * 80000);
@@ -10,13 +10,37 @@ public class ShellSort {
         long start = System.currentTimeMillis();
         System.out.println("排序前的时间为：" + start);
 
-        shellSort(arr);
+        shellSort2(arr);
 
         // 排序后的时间
         long end = System.currentTimeMillis();
         System.out.println("排序后的时间为：" + end);
         System.out.println("时间为：" + (end - start));
 
+
+    }
+
+    public static void shellSort2(int[] arr) {
+        // 增量gap,逐步缩小
+        for (int gap = arr.length/2;gap>0;gap/=2){
+            // 从
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j]<arr[j-gap]){
+                    while (j-gap>=0&&temp<arr[j-gap]){
+                        // 后移
+                        arr[j] = arr[j-gap];
+                        j -= gap;
+
+
+                    }
+                    if (temp!=arr[j]){
+                        arr[j] =temp;
+                    }
+                }
+            }
+        }
     }
 
     public static void shellSort(int[] arr) {
